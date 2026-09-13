279955d2678648dee63c7142aa56e3796121c9b1
Emad Jaber <emadjbr2003@gmail.com>
2026-09-13 22:53:01 +0300

    First commit - DVLD Base Project

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=DVLD_Grad;User Id=sa;Password=123456;";
+
+
+    }
+}
