d46dc1b9aa728074069c13dfe65a60991cc92a41
Emad Jaber <emadjbr2003@gmail.com>
2026-09-15 16:44:12 +0300

    fix: configure desktop SqlClient dependencies

--- DVLD_Buisness/clsLocalDrivingLicenseApplication.cs
+++ DVLD_Buisness/clsLocalDrivingLicenseApplication.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Data;
-using System.Runtime.Remoting.Messaging;
 using System.Security.Cryptography;
 using System.Xml.Linq;
 using DVLD_DataAccess;
--- DVLD_DataAccess/clsDataAccessSettings.cs
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -4,7 +4,7 @@ namespace DVLD_DataAccess
 {
     static class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=DVLD_Grad;User Id=sa;Password=123456;";
+        public static string ConnectionString = "Server=.;Database=DVLD_Grad;User Id=sa;Password=123456;TrustServerCertificate=True;";
 
 
     }
