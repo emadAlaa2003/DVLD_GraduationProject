12446ce3e4f1065d2f99cc1507a3d11e1fd68868
Emad Jaber <emadjbr2003@gmail.com>
2026-09-15 10:14:03 +0300

    feat: add API health endpoint and remove weather sample

---
+++ DVLD.Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DVLD.Api.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                service = "DVLD.Api",
+                status = "Running"
+            });
+        }
+    }
+}
